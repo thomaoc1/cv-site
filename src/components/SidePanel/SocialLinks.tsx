a001b469eb3f06c333ec5afca2eee4c48c854fd0
thomaoc <thomas.ocuilleanain@ulb.be>
2024-08-08 19:30:59 +0200

    last thing

--- src/components/SidePanel/SocialLinks.tsx
+++ src/components/SidePanel/SocialLinks.tsx
@@ -5,12 +5,12 @@ function SocialLinks() {
         <div className='social-links'>
             <a href='https://www.linkedin.com/in/thomas-o-cuilleanain-02205a241/'>
                 <button className='invisible-button'>
-                    <img className='social-logo' src='/linked-in-logo.png' alt='LinkedIn logo' />
+                    <img className='social-logo' src='linked-in-logo.png' alt='LinkedIn logo' />
                 </button>
             </a>
             <a href='https://github.com/thomaoc1' >
                 <button className='invisible-button'>
-                    <img className='social-logo' id='github-logo' src='/github-logo.png' alt='Github logo'/>
+                    <img className='social-logo' id='github-logo' src='github-logo.png' alt='Github logo'/>
                 </button>
             </a>
         </div>
