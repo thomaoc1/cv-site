40974bb3c8c29c319300c4986168a06cd08403da
thomaoc <thomas.ocuilleanain@ulb.be>
2024-08-09 15:51:37 +0200

    centered name and description

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,29 +2,15 @@ import './PageStyle.css'
 import SidePanel from "../SidePanel/SidePanel.tsx";
 import ResumeContainer from "../Resume/ResumeContainer.tsx";
 import { useState } from "react";
-import {useSwipeable} from "react-swipeable";
+import { useSwipeable } from "react-swipeable";
 
 function Page() {
 
     const [isSidebarOpen, setSidebarOpen] = useState(false);
     const handlers = useSwipeable({
-        onSwipedLeft: () => {
-            if (isSidebarOpen) {
-                setSidebarOpen(false);
-            }
-        },
-        onSwipedRight: () => {
-            if (!isSidebarOpen) {
-                setSidebarOpen(true);
-            }
-        },
+        onSwipedLeft: () => { setSidebarOpen(false); },
+        onSwipedRight: () => { setSidebarOpen(true); }
     })
-
-    const onContainerClicked = () => {
-        if (isSidebarOpen) {
-            setSidebarOpen(false);
-        }
-    }
 
     return (
         <div className='page' {...handlers}>
@@ -35,7 +21,7 @@ function Page() {
             </div>
             <div className='main-page-content'>
                 <SidePanel isOpen={isSidebarOpen}/>
-                <ResumeContainer isSidebarOpen={isSidebarOpen} onContainerClicked={onContainerClicked} />
+                <ResumeContainer isSidebarOpen={isSidebarOpen} onContainerClicked={() => setSidebarOpen(false)} />
             </div>
         </div>
     )
--- src/components/SidePanel/Profile.tsx
+++ src/components/SidePanel/Profile.tsx
@@ -4,10 +4,8 @@ function Profile() {
     return (
         <div className='profile-section'>
             <img className='profile-picture' src='profile_picture.png' alt='Profile Picture'/>
-            <div>
-                <p className='profile-name'> Thomas O'Cuilleanain </p>
-                <p className='profile-description'> Machine Learning Engineer <br/> Fullstack Developer </p>
-            </div>
+            <p className='profile-name'> Thomas O'Cuilleanain </p>
+            <p className='profile-description'> Machine Learning Engineer <br/> Fullstack Developer </p>
         </div>
     )
 }
